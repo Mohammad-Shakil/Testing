fa1436d8aaf7df8815f52cfc8cb1254436e62388
Shakil <md.fahim.sakil@gmail.com>
2026-09-23 19:25:01 +0600

    this was tough string reversal

--- main.go
+++ main.go
@@ -2,47 +2,22 @@ package main
 
 import "fmt"
 
-type Account struct {
-	Owner   string
-	Balance int
-}
+func stringreverse(p string) string {
 
-func findAccount(users []Account, name string) int {
-	for i := 0; i < len(users); i++ {
-		if users[i].Owner == name {
-			return i
-		}
+	runes := []int32(p)
+
+	low := 0
+	high := len(runes) - 1
+
+	for low < high {
+		runes[low], runes[high] = runes[high], runes[low]
+		low++
+		high--
 	}
-	return -1
+	return string(runes)
 }
 
 func main() {
 
-	users := []Account{
-		{Owner: "Shakil", Balance: 4000},
-		{Owner: "Fahad", Balance: 5000},
-		{Owner: "Rahim", Balance: 7000},
-	}
-
-	user := findAccount(users, "Shakil")
-	if user != -1 {
-		users[user].Balance += 500
-		fmt.Printf("\n%s balance: %d", users[user].Owner, users[user].Balance)
-	}
-
-	user = findAccount(users, "Rahim")
-	ammount := 60000
-	if user != -1 {
-		if users[user].Balance >= ammount {
-			users[user].Balance -= ammount
-			fmt.Printf("\n%s balance: %d", users[user].Owner, users[user].Balance)
-		} else {
-			fmt.Println(" \ninffsfficient money")
-		}
-	}
-
-	for _, value := range users {
-		fmt.Printf("\n%s users balance is: %d", value.Owner, value.Balance)
-	}
-
+	fmt.Println(stringreverse("shakil"))
 }
